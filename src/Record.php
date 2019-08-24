7022795e7656a5c308f9086a06e337c6b7f00973
Tom Schlick <tschlick@orisintel.com>
2019-08-24 15:17:25 -0400

    add record types

---
+++ src/Record.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace TomSchlick\DoH;
+
+
+class Record
+{
+    public const TYPE_A = 1;
+    public const TYPE_AAAA = 28;
+    public const TYPE_CAA = 257;
+    public const TYPE_CNAME = 5;
+    public const TYPE_DS = 43;
+    public const TYPE_KEY = 25;
+    public const TYPE_MX = 15;
+    public const TYPE_NS = 2;
+    public const TYPE_PTR = 12;
+    public const TYPE_SOA = 6;
+    public const TYPE_SRV = 33;
+    public const TYPE_TXT = 16;
+}
