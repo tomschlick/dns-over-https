1bbb388654ab48908a3d5208886e1bcee18b217a
Tom Schlick <tom@schlick.email>
2019-10-20 20:36:09 -0400

    add endpoints for google/cloudflare

--- src/Providers/Cloudflare.php
+++ src/Providers/Cloudflare.php
@@ -4,4 +4,10 @@ namespace TomSchlick\DoH\Providers;
 
 class Cloudflare extends ProviderBase
 {
+    public function endpoints(): array
+    {
+        return [
+            'https://cloudflare-dns.com/dns-query'
+        ];
+    }
 }
--- src/Providers/Google.php
+++ src/Providers/Google.php
@@ -4,4 +4,11 @@ namespace TomSchlick\DoH\Providers;
 
 class Google extends ProviderBase
 {
+
+    public function endpoints(): array
+    {
+        return [
+            'https://dns.google/resolve',
+        ];
+    }
 }
