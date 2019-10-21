8094c554e66998ec7848b2c97a2e9aab50c50081
Tom Schlick <tom@schlick.email>
2019-10-20 20:35:45 -0400

    remove unnessesary methods

--- src/Providers/ProviderBase.php
+++ src/Providers/ProviderBase.php
@@ -2,11 +2,10 @@
 
 namespace TomSchlick\DoH\Providers;
 
+
 abstract class ProviderBase
 {
     abstract public function endpoints() : array;
 
-    abstract public function ipv4Addresses() : ?array;
 
-    abstract public function ipv6Addresses() : ?array;
 }
