e3032c7779cfbdd5395db82138088d2fc3a70c50
Tom Schlick <tom@schlick.email>
2019-10-20 20:35:56 -0400

    fetch records

--- src/Providers/ProviderBase.php
+++ src/Providers/ProviderBase.php
@@ -2,10 +2,39 @@
 
 namespace TomSchlick\DoH\Providers;
 
+use GuzzleHttp\Client as GuzzleClient;
+use TomSchlick\DoH\QueryReponse;
+use TomSchlick\DoH\RecordType;
+use function GuzzleHttp\Psr7\build_query;
 
 abstract class ProviderBase
 {
     abstract public function endpoints() : array;
 
+    public function fetch(string $name, int $type) : QueryReponse
+    {
+        $recordType = new RecordType($type);
 
+        $options = [
+            'headers' => ['accept' => 'application/dns-json'],
+        ];
+
+        foreach($this->endpoints() as $endpoint) {
+            $url = $endpoint . '?' . build_query([
+                    'name' => $name,
+                    'type' => $recordType->description
+                ]);
+
+            $response = $this->getHttpClient()->get($url, $options);
+
+            if($response->getStatusCode() == 200) {
+                return new QueryReponse($response->getBody());
+            }
+        }
+    }
+
+    public function getHttpClient()
+    {
+        return new GuzzleClient();
+    }
 }
