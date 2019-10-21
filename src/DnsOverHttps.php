2063c57bcfb49fbfb55752654b1185e0b9ebe02f
Tom Schlick <tom@schlick.email>
2019-10-20 20:36:24 -0400

    add methods

--- src/DnsOverHttps.php
+++ src/DnsOverHttps.php
@@ -2,6 +2,27 @@
 
 namespace TomSchlick\DoH;
 
+use TomSchlick\DoH\Providers\ProviderBase;
+
 class DnsOverHttps
 {
+    /**
+     * @var \TomSchlick\DoH\Providers\ProviderBase
+     */
+    protected $provider;
+
+    public function __construct(ProviderBase $provider)
+    {
+        $this->setProvider($provider);
+    }
+
+    public function setProvider(ProviderBase $provider) : void
+    {
+        $this->provider = $provider;
+    }
+
+    public function fetch(string $name, int $type)
+    {
+        return $this->provider->fetch($name, $type);
+    }
 }
