3d9b79d7dce2053e9d3b4c1e49f75b8c0926840f
Tom Schlick <tom@schlick.email>
2019-10-20 20:34:42 -0400

    Create StatusType.php

---
+++ src/StatusType.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace TomSchlick\DoH;
+
+use BenSampo\Enum\Enum;
+
+class StatusType extends Enum
+{
+    public const NO_ERROR = 0;
+    public const FORMAT_ERROR = 1;
+    public const SERVER_FAILURE = 2;
+    public const NON_EXISTANT_DOMAIN = 3;
+    public const NOT_IMPLEMENTED = 4;
+}
