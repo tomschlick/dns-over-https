dade18d6cf6d4804b8d5f864444b31e32c9b32e3
Tom Schlick <tom@schlick.email>
2019-10-20 20:36:29 -0400

    Update ExampleTest.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,18 +5,32 @@ declare(strict_types=1);
 namespace TomSchlick\DoH\Tests;
 
 use PHPUnit\Framework\TestCase;
+use TomSchlick\DoH\DnsOverHttps;
+use TomSchlick\DoH\Providers\Cloudflare;
+use TomSchlick\DoH\Providers\Google;
+use TomSchlick\DoH\RecordType;
+use TomSchlick\DoH\StatusType;
 
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function test_cloudflare()
     {
-        $this->assertTrue(true);
+        $doh = new DnsOverHttps(new Cloudflare());
+
+        $result = $doh->fetch('tomschlick.com', RecordType::A);
+
+        $this->assertEquals(StatusType::NO_ERROR, $result->getStatus()->value);
     }
 
     /** @test */
-    public function true_is_true2()
+    public function test_google()
     {
-        $this->assertTrue(true);
+        $doh = new DnsOverHttps(new Google());
+
+        $result = $doh->fetch('tomschlick.com', RecordType::A);
+
+        $this->assertEquals(StatusType::NO_ERROR, $result->getStatus()->value);
     }
+
 }
